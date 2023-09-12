ce8cc3a42b04f9abc8e85e5c88de3e7e47c614b1
Aaron <avnguyen08@gmail.com>
2023-09-12 15:17:30 -0700

    Integrated Sleep Cycle fo Servo and ESP32, fully functional outdoors

--- Auto_Solar_Panel_Code/Auto_Solar_Panel_Code.ino
+++ Auto_Solar_Panel_Code/Auto_Solar_Panel_Code.ino
@@ -7,12 +7,13 @@ Uses D2 and D4 as PWM pins to configure servo motors for solar panels.
 #include <ESP32Servo.h>
 #include "Servo_Panel_Calibration\servo_move.h"
 //DEBUG VARIABLES
-#define SLOW 50
-#define MEDIUM 25
+#define SUPER_SLOW 400
+#define SLOW 100
+#define MEDIUM 50
 #define FAST 12
 #define SUPERFAST 5
 char string[50];
-
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 
 //value of photoresistors
 int topleft;
@@ -29,38 +30,59 @@ int PINA = 25;
 int PINB = 33;
 int PINC = 32;
 int PIND = 35;
+//Pin to control Power switch
+int PIN_Power = 14;
 
 Servo myservoBot;  // create servo object to control bottom servo
 Servo myservoTop;  // create servo object to control top servo
 // 16 servo objects can be created on the ESP32
 int minbotval = 0;
 int maxbotval = 180;
-int mintopval = 0;
-int maxtopval = 180;
-uint posTop = 90;  // variable to store the servo position
-uint posBot = 90;  // variable to store the servo position
+int mintopval = 45;
+int maxtopval = 135;
+RTC_DATA_ATTR uint posTop = 90;       // variable to store the top servo position, RTC saves after sleep
+RTC_DATA_ATTR uint posBot = 90;       // variable to store the bottom servo position, RTC saves after sleep
+uint tempTop = 90;  //variable to store past top position
+uint tempBot = 90;  //variable to store past bot position
+int startTime = 0;
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
 int servoPinTop = 4;
 int servoPinBot = 2;
+
+
+
+  //Make solar panel servos and esp32 go to sleep for inputted amount of time
+void solar_sleep(int slp_time) {
+  digitalWrite(PIN_Power, LOW);
+  Serial.print("Going into sleep mode for ");
+  Serial.print(slp_time);
+  Serial.println(" seconds");
+  //Turn off ESP32
+  esp_sleep_enable_timer_wakeup(slp_time*uS_TO_S_FACTOR); //enables wake up by timer
+  esp_deep_sleep_start(); //starts deep sleep mode. RTC timer + RTC memorys burns only 10uA
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(PINA, INPUT);
   pinMode(PINB, INPUT);
   pinMode(PINC, INPUT);
   pinMode(PIND, INPUT);
+  pinMode(PIN_Power, OUTPUT);
+  digitalWrite(PIN_Power, HIGH);
   // Allow allocation of all timers
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
   ESP32PWM::allocateTimer(2);
   ESP32PWM::allocateTimer(3);
   //Using 70hz PWM signal. May differ based on servo
-  myservoBot.setPeriodHertz(70);              // standard 50 hz servo
-  myservoTop.setPeriodHertz(70);              // standard 50 hz servo
+  myservoBot.setPeriodHertz(70);  // standard 50 hz servo
+  myservoTop.setPeriodHertz(70);  // standard 50 hz servo
   // using min/max of 500us and 2400us. May differ based on servo
   myservoBot.attach(servoPinBot, 500, 2400);  // attaches the servo on pin 2 to the servo object
   myservoTop.attach(servoPinTop, 500, 2400);  // attaches the servo on pin 4 to the servo object
-  servo_move(&myservoBot, &posBot, 90, FAST);
-  servo_move(&myservoTop, &posTop, 90, FAST);
+  // servo_move(&myservoBot, &posBot, 90, FAST);
+  // servo_move(&myservoTop, &posTop, 90, FAST);
   Serial.println("Servo Setup Finished!");
   delay(1500);
 }
@@ -70,39 +92,50 @@ void loop() {
   topright = analogRead(PINB);
   downleft = analogRead(PINC);
   downright = analogRead(PIND);
+  top = (topleft + topright) / 2;
+  down = (downleft + downright) / 2;
+  left = (topleft + downleft) / 2;
+  right = (topright + downright) / 2;
 
-  top = (topleft + topright)/2;
-  down = (downleft + downright)/2;
-  left = (topleft + downleft)/2;
-  right = (topright + downright)/2;
+  //edge case: sensors in darkness, no point in moving servos because it may be a cloud. ESP32 max value is 4096 for analog read
+  if (top > 4050 && down > 4050) {
+    // servo_move(&myservoTop, &posTop, 90, FAST);
+    // servo_move(&myservoBot, &posBot, 90, FAST);
+  } else {
+    int currentTime = millis();  // current time
+    //If last position of servos has been the same for a x amount of time, activate sleep mode
+    if (currentTime - startTime >= 3000) {
+      if (tempTop == posTop && tempBot == posBot) {
+        tempTop = posTop;
+        tempBot = posBot;
+        solar_sleep(3); //sleep for x amount of seconds
+      } else {
+        startTime = currentTime;
+        tempTop = posTop;
+        tempBot = posBot;
+      }
+    } else
+      //light shines on right side more than left.  Turn counterclockwise
+      if (((left - right) > light_sens) && (posBot < maxbotval)) {
+        posBot = posBot + change;
+      }
 
-  //edge case: sensors in pure darkness, point solar panel up so get sensors to read again. ESP32 max value is 4096 for analog read
-  if(top > 4050 && down > 4050){
-  servo_move(&myservoTop, &posTop, 90, FAST);
-  servo_move(&myservoBot, &posBot, 90, FAST);
+    //light shines on left side more than right. Turn clockwise
+    if (((right - left) > light_sens) && (posBot > minbotval)) {
+      posBot = posBot - change;
+    }
+
+    //light shines on down side more than top side. Turn clockwise
+    if (((top - down) > light_sens) && (posTop < maxtopval)) {
+      posTop = posTop + change;
+    }
+
+    //light shines on top side more than down side. Turn counterclockwise
+    if (((down - top) > light_sens) && (posTop > mintopval)) {
+      posTop = posTop - change;
+    }
   }
-
-  //light shines on right side more than left.  Turn counterclockwise
-  if (((left - right) > light_sens) && (posBot < maxbotval)) {
-    posBot = posBot + change;
-  }
-
-  //light shines on left side more than right. Turn clockwise
-  if (((right - left) > light_sens) && (posBot > minbotval)) {
-    posBot = posBot - change;
-  }
-
-  //light shines on down side more than top side. Turn clockwise
-  if (((top - down) > light_sens) && (posTop < maxtopval)) {
-    posTop = posTop + change;
-  }
-
-  //light shines on top side more than down side. Turn counterclockwise
-  if (((down - top) > light_sens) && (posTop > mintopval)) {
-    posTop = posTop - change;
-  }
-
-    delay(30);
+  delay(MEDIUM);
   //debug string meant to display servos' degrees and LDR sensor readings
   sprintf(string, "Bottom Servo: %u \t\t Top Servo: %u \t\t LDR: %d \t\t%d\t\t%d\t\t%d", posBot, posTop, topleft, topright, downleft, downright);
   Serial.println(string);
